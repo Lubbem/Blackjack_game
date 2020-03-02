59b283512b1ce1e6963f6e12e14d7f591ab74706
FrossouwDVT <frossouw@jhb.dvt.co.za>
2020-03-02 13:14:48 +0200

    Created card class

---
+++ build/Classes/Card.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Card = /** @class */ (function () {
+    function Card(name, value, suit) {
+        this.name = name;
+        this.value = value;
+        this.suit = suit;
+    }
+    return Card;
+}());
+exports.Card = Card;
---
+++ src/Classes/Card.ts
@@ -0,0 +1,5 @@
+export class Card {
+
+    constructor(public name: string, public value: number[], public suit: string) {} 
+
+}
