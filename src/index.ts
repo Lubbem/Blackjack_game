2999347294f40302487da5375099144a3094d367
FrossouwDVT <frossouw@jhb.dvt.co.za>
2020-03-02 13:16:26 +0200

    Implemented changes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import { FileHandler } from "./FileHandling/FileHandler";
 import { CSVFileReader } from "./FileHandling/CSVReader";
+import { TXTFileReader } from "./FileHandling/TxtReader";
+import { Card } from "./Classes/Card";
+import { Deck } from "./Classes/Deck";
+import { Round } from "./Classes/Round";
 
+const txtReader = new TXTFileReader();
+const deck = new Deck();
 
-const csvReader = new CSVFileReader();
-const fh = new FileHandler('test.csv', csvReader);
+const fh = new FileHandler('test.txt', txtReader);
 var data: string[][] = fh.fileRead();
 
 if (fh.dataReadSuccess) {
-    console.log(data);
+    //Each row within data is a new round
+    for (let i = 0; i < data.length; i++) {
+        console.log(`=======================================`);
+        console.log(` Round ${i+1} Started`);
+        console.log(`=======================================`);
+        var round = new Round(data[i]);
+
+        console.log('\n');
+    }
+
 } else {
-    console.log("No data has been retrieved, an error occurred.");   
+    console.log("No data has been retrieved, an error occurred.");
 }
 
 
